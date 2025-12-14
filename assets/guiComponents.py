c11421ec6e57fd40b7db0ca26352d8c7f75ae3ef
Marcus-Crump <mlc5638@eid.utexas.edu>
2025-12-14 16:55:29 +0900

    set up basics for window class and functions for the main file. Will most likely change all of this later to be more organized

--- assets/guiComponents.py
+++ assets/guiComponents.py
@@ -2,7 +2,12 @@ import os, sys
 import PyQt5.QtWidgets as Q
 
 class MainWindow(Q.QMainWindow):
-    def __init__(self):
+    def __init__(self, op, path, items):
         super().__init__()
+        self.__system = op
         self.setWindowTitle("Finder")
         self.setGeometry(500,500,500,500)
+    
+    @property
+    def system(self):
+        return self.__system
--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-import os, sys
-import assets.guiComponents
+import os, sys, subprocess, platform
+import assets.guiComponents as GUI
+from PyQt5.QtWidgets import QApplication
+
+def get_starting_path(system):
+    if system == "Windows":
+        return os.path.expanduser("%HOMEPATH%").strip()
+    else:
+        return os.path.expanduser("~/").strip()
+
+def initialize():
+    system = platform.system()
+    start = get_starting_path(system)
+    items = subprocess.check_output(["ls",start],text=True).strip().split()
+    return system, start, items
 
 def main():
-    pass
+    app = QApplication(sys.argv)
+    op,path,items = initialize()
+    window = GUI.MainWindow(op, path, items)
+    window.show()
+
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
     main()
